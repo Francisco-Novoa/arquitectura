2a038cb706331082bf1ace2566c229836b7e542a
pancho <fj.novoap@gmail.com>
2022-06-01 17:41:13 -0400

    fixed model imports

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 import { DataTypes } from "sequelize";
+import sequelize from "../database/database.js";
 
 import { User } from "./users.js";
 import { Perfil } from "./perfil.js";
@@ -6,8 +7,9 @@ import { Menu } from "./menu.js";
 import { Salud } from "./salud.js";
 import { Problema_Salud } from "./problema_salud.js";
 import { Inscritos } from "./inscritos.js";
-
-import sequelize from "../database/database.js";
+import { Actividad } from "./actividad.js";
+import { Mensajeria } from "./mensajeria.js";
+import { Ruta } from "./ruta.js";
 
 //esto define relaciones
 User.belongsTo(Perfil, {
@@ -17,12 +19,24 @@ User.belongsTo(Perfil, {
     allowNull: false,
   },
 });
+
 Perfil.hasMany(User);
 Perfil.belongsToMany(Menu, { through: "menu_perfil" });
 Menu.belongsToMany(Perfil, { through: "menu_perfil" });
-Salud.belongsTo(User)
-User.hasOne(Salud)
-Salud.hasMany(Problema_Salud)
+Salud.belongsTo(User);
+User.hasOne(Salud);
+Salud.hasMany(Problema_Salud);
+
 await sequelize.sync({ force: true });
 
-export { User, Perfil, Menu, Salud, Problema_Salud };
+export {
+  User,
+  Perfil,
+  Menu,
+  Salud,
+  Problema_Salud,
+  Actividad,
+  Inscritos,
+  Mensajeria,
+  Ruta,
+};
