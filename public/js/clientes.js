f60dbd4467c68550f462b25615f454ded52769bc
pancho <fj.novoap@gmail.com>
2022-07-02 01:01:19 -0400

    load and update user working

--- public/js/clientes.js
+++ public/js/clientes.js
@@ -1,119 +1,123 @@
 window.onload = async () => {
-    //   const login = await axios.post("/api/login", {
-    //     correo: "pancho",
-    //     password: "1234567890",
-    //   });
-  
-    const tablaClientes = document.getElementById("tabla-clientes");
-    const bottonCrearCliente = document.getElementById(`crear-cliente-button`);
-  
-    const llenarMensaje = id => async mensaje => {
-      const label = (document.getElementById(`message-cliente-${id}`).innerHTML =
-        mensaje.response.data.error);
-    };
-  
-    // const crearCliente = async () => {
-      const password = document.getElementById(`crear-cliente-password`);
-      const rut = document.getElementById(`crear-cliente-rut`);
-      const correo = document.getElementById(`crear-cliente-correo`);
-      const fecha_nacimiento = document.getElementById(`crear-cliente-fecha_nacimiento`);
-      const genero = document.getElementById(`crear-cliente-genero`);
-      const fono = document.getElementById(`crear-cliente-fono`);
-      const direccion = document.getElementById(`crear-cliente-direccion`);
-      const fono_emergencia = document.getElementById(`crear-cliente-fono_emergencia`);
-      const nombre_emergencia = document.getElementById(`crear-cliente-nombre_emergencia`);
-      const estado = document.getElementById(`crear-cliente-estado`);
-      const derivar_a = document.getElementById(`crear-cliente-derivar_a`);
-  
-    //   try {
-    //     const result = await axios.post("api/users", {
-            // nombre: nombre.value,
-            // password: password.value,
-            // rut: rut.value,
-            // correo: correo.value,
-            // fecha_nacimiento: fecha_nacimiento.value,
-            // genero: genero.value,
-            // fono: fono.value,
-            // direccion: direccion.value,
-            // fono_emergencia: fono_emergencia.value,
-            // nombre_emergencia: nombre_emergencia.value,
-            // estado: estado.value,
-            // derivar_a: derivar_a.value,
-    //     });
-    //     password.value = "";
-    //     rut.value = "";
-    //     correo.value = "";
-    //     fecha_nacimiento.value = "";
-    //     genero.value = "";
-    //     fono.value = "";
-    //     direccion.value = "";
-    //     fono_emergencia.value = "";
-    //     nombre_emergencia.value = "";
-    //     estado.value = "";
-    //     derivar_a.value = "";
-    //     loadClientes();
-    //   } catch (error) {
-    //     alert(error);
-    //   }
-    // };
-  
-    // bottonCrearCliente.addEventListener("click", crearCliente);
-  
-    const editarCliente = id => async () => {
-      const nombre = document.getElementById(`input-cliente-edit-${id}`);
-      const password = document.getElementById(`crear-cliente-password`);
-      const rut = document.getElementById(`crear-cliente-rut`);
-      const correo = document.getElementById(`crear-cliente-correo`);
-      const fecha_nacimiento = document.getElementById(`crear-cliente-fecha_nacimiento`);
-      const genero = document.getElementById(`crear-cliente-genero`);
-      const fono = document.getElementById(`crear-cliente-fono`);
-      const direccion = document.getElementById(`crear-cliente-direccion`);
-      const fono_emergencia = document.getElementById(`crear-cliente-fono_emergencia`);
-      const nombre_emergencia = document.getElementById(`crear-cliente-nombre_emergencia`);
-      const estado = document.getElementById(`crear-cliente-estado`);
-      const derivar_a = document.getElementById(`crear-cliente-derivar_a`);
-      
-      try {
-        const result = await axios.put(`/api/users/${id}`, {
-            nombre: nombre.value,
-            password: password.value,
-            rut: rut.value,
-            correo: correo.value,
-            fecha_nacimiento: fecha_nacimiento.value,
-            genero: genero.value,
-            fono: fono.value,
-            direccion: direccion.value,
-            fono_emergencia: fono_emergencia.value,
-            nombre_emergencia: nombre_emergencia.value,
-            estado: estado.value,
-            derivar_a: derivar_a.value,
+  //   const login = await axios.post("/api/login", {
+  //     correo: "pancho",
+  //     password: "1234567890",
+  //   });
 
-        });
-        loadClientes();
-      } catch (error) {
-        llenarMensaje(id)(error);
-      }
-    };
-  
-    const eliminarBanco = id => async () => {
-      try {
-        const result = await axios.delete(`/api/banco/${id}`);
-        loadBancos();
-      } catch (error) {
-        llenarMensaje(id)(error.data.message);
-      }
-    };
-  
-    const loadClientes = async () => {
-      const clientes = await axios.get("/api/users");
-      tablaClientes.innerHTML = "";
-      clientes.data.data.cliente.map(cliente => {
-        const tableRow = document.createElement("tr");
-        tableRow.innerHTML = `
-          <td>${cliente.password}</td>
+  const bottonEditarCliente = document.getElementById(`cliente-update-button`);
+
+  const llenarMensaje = id => async mensaje => {
+    const label = (document.getElementById(id).innerHTML =
+      mensaje.response.data.error);
+  };
+
+  // const crearCliente = async () => {
+  //   try {
+  //     const result = await axios.post("api/users", {
+  // nombre: nombre.value,
+  // password: password.value,
+  // rut: rut.value,
+  // correo: correo.value,
+  // fecha_nacimiento: fecha_nacimiento.value,
+  // genero: genero.value,
+  // fono: fono.value,
+  // direccion: direccion.value,
+  // fono_emergencia: fono_emergencia.value,
+  // nombre_emergencia: nombre_emergencia.value,
+  // estado: estado.value,
+  // derivar_a: derivar_a.value,
+  //     });
+  //     password.value = "";
+  //     rut.value = "";
+  //     correo.value = "";
+  //     fecha_nacimiento.value = "";
+  //     genero.value = "";
+  //     fono.value = "";
+  //     direccion.value = "";
+  //     fono_emergencia.value = "";
+  //     nombre_emergencia.value = "";
+  //     estado.value = "";
+  //     derivar_a.value = "";
+  //     loadClientes();
+  //   } catch (error) {
+  //     alert(error);
+  //   }
+  // };
+
+  // bottonCrearCliente.addEventListener("click", crearCliente);
+
+  const id = document.getElementById(`input-id`);
+  const nombre = document.getElementById(`input-nombre`);
+  const rut = document.getElementById(`input-rut`);
+  const correo = document.getElementById(`input-correo`);
+  const fecha_nacimiento = document.getElementById(`input-fecha_nacimiento`);
+  const genero = document.getElementById(`input-genero`);
+  const fono = document.getElementById(`input-fono`);
+  const direccion = document.getElementById(`input-direccion`);
+  const fono_emergencia = document.getElementById(`input-fono_emergencia`);
+  const nombre_emergencia = document.getElementById(`input-nombre_emergencia`);
+  const estado = document.getElementById(`input-estado`);
+  const derivar_a = document.getElementById(`input-derivar_a`);
+
+  const editarCliente = async () => {
+    const msgFunction = llenarMensaje("cliente-update-button-label");
+    try {
+      const result = await axios.put(`/api/users/${id.value}`, {
+        nombre: nombre.value,
+        rut: rut.value,
+        correo: correo.value,
+        fecha_nacimiento: fecha_nacimiento.value,
+        genero: genero.value,
+        fono: fono.value,
+        direccion: direccion.value,
+        fono_emergencia: fono_emergencia.value,
+        nombre_emergencia: nombre_emergencia.value,
+        estado: estado.value,
+        derivar_a: derivar_a.value,
+      });
+      loadClientes();
+    } catch (error) {
+      console.error(error);
+      msgFunction(error);
+    }
+  };
+
+  bottonEditarCliente.addEventListener("click", editarCliente);
+
+  // const eliminarBanco = id => async () => {
+  //   try {
+  //     const result = await axios.delete(`/api/banco/${id}`);
+  //     loadBancos();
+  //   } catch (error) {}
+  // };
+
+  const tablaClientes = document.getElementById("tabla-clientes");
+
+  const loadData = user => () => {
+    console.log(user);
+    id.value = user.id;
+    nombre.value = user.nombre;
+    correo.value = user.correo;
+    rut.value = user.rut;
+    fecha_nacimiento.value = user.fecha_nacimiento;
+    genero.value = user.genero;
+    fono.value = user.fono;
+    direccion.value = user.direccion;
+    fono_emergencia.value = user.fono_emergencia;
+    nombre_emergencia.value = user.nombre_emergencia;
+    estado.value = user.estado;
+    derivar_a.value = user.derivar_a;
+  };
+
+  const loadClientes = async () => {
+    const clientes = await axios.get("/api/users");
+    tablaClientes.innerHTML = "";
+    clientes.data.data.user.map(cliente => {
+      const tableRow = document.createElement("tr");
+      tableRow.innerHTML = `
+          <td>${cliente.nombre}</td>
           <td>${cliente.rut}</td>
           <td>${cliente.correo}</td>
-          <td>${cliente.nombre}</td>
           <td>${cliente.fecha_nacimiento}</td>
           <td>${cliente.genero}</td>
           <td>${cliente.fono}</td>
@@ -124,30 +128,17 @@ window.onload = async () => {
           <td>${cliente.derivar_a}</td>
           <td>${cliente.createdAt}</td>
           <td>${cliente.updatedAt}</td>
-
           <td>
-          <div class="ordenado">
-          <label class="error" id="message-cliente-${cliente.rut}"> 
-          </label >
-          </div>
-              <div class="ordenado">
-                  <button id="button-send-cliente-edit-${cliente.rut}" >Editar</button>
-                  <input type="text" id="input-cliente-edit-${cliente.rut}">
-              </div>
+            <button id="button-load-cliente-${cliente.rut}" >Cargar</button>
           </td>
-          <td>
-          <button id="button-send-cliente-delete-${cliente.rut}" >Eliminar</button>
-          </td>`;
-        tablaclientes.appendChild(tableRow);
-        document
-          .getElementById(`button-send-cliente-delete-${cliente.rut}`)
-          .addEventListener("click", eliminarCliente(cliente.rut));
-        document
-          .getElementById(`button-send-cliente-edit-${cliente.rut}`)
-          .addEventListener("click", editarCliente(cliente.rut));
-      });
-    };
-  
-    loadClientes();
+          
+          `;
+      tablaClientes.appendChild(tableRow);
+      document
+        .getElementById(`button-load-cliente-${cliente.rut}`)
+        .addEventListener("click", loadData(cliente));
+    });
   };
-  
+
+  loadClientes();
+};
